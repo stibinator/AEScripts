e2be0a98f19967ff6566a40fbd4946777b718b6e
stib <stib@pureandapplied.com.au>
2018-02-21 13:02:29 +1100

    proxynator is ready to use

--- proxynator.jsx
+++ proxynator.jsx
@@ -1,16 +1,20 @@
-﻿var originals = app.project.selection;
-var f, proxyPath;
-var proxyFolder = Folder.selectDialog ("choose the source folder for the proxies");
-for (i=1; i <= originals.length; i++){
-     try {
-   if (originals[i].typeName==="Footage"){
+var originals = app.project.selection;
+if (originals) {
+  var f,
+    proxyPath;
+  var proxyFolder = Folder.selectDialog("Choose the source folder for the proxies");
+  for (i = 1; i <= originals.length; i++) {
+    try {
+      if (originals[i].typeName === "Footage") {
         newProxy = new File(proxyFolder.absoluteURI + "/" + originals[i].file.name);
-      
-           originals[i].setProxy(newProxy);
-           }
-      
-       }
-    catch(e){
-           writeln(e)
-           }
-   }
+
+        originals[i].setProxy(newProxy);
+      }
+
+    } catch (e) {
+      writeln(e)
+    }
+  }
+} else {
+  alert("Choose some footage items to add proxies to")
+}
