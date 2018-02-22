b6d0b055fed1a0e1b1894ddb68015cfa90ce424d
stib <stib@pureandapplied.com.au>
2018-02-22 17:57:57 +1100

    measureRenderQueue works with a shell script to find out how many items there are in a render queue

---
+++ measureRenderQueue.jsx
@@ -0,0 +1,7 @@
+alert(renderProj);
+app.open(renderProj);
+var rq = app.project.renderQueue;
+var tempFile = new File("~/Appdata/Local/Temp/rqcount.txt");
+tempFile.open("w");
+tempFile.write(rq.numItems);
+tempFile.close();
