817b303c68c1f0a4d39905d80e94b5c88ef599c3
stib <stib@pureandapplied.com.au>
2018-03-22 14:24:48 +1100

    Added UI to proxinator, did a bit of testing, seems to be ready for release

--- ScriptUI Panels/proxynator.jsx
+++ ScriptUI Panels/proxynator.jsx
@@ -1,11 +1,15 @@
-var proxyFolder;
+// global constants
+var scriptName = "proxinator";
+var prefsFile = Folder.userData.absoluteURI + "/AE_proxinator.prefs";
 
-function proxinate(proxyFolder) {
-  var originals = app.project.selection;
+function proxinate(proxyFolder, items) {
+  // set a proxy for all selected items by matching file names in a folder
+  originals = items || app.project.selection;
+  var msg = "";
   if (originals) {
     var f,
       proxyPath;
-    for (i = 0; i < originals.length; i++) {
+    for (var i = 0; i < originals.length; i++) {
       if (!proxyFolder) {
         chooseProxyFolder()
       }
@@ -14,37 +18,76 @@ function proxinate(proxyFolder) {
           newProxy = new File(proxyFolder.absoluteURI + "/" + originals[i].file.name);
           originals[i].setProxy(newProxy);
         } else {
-          newProxy = new File(proxyFolder.absoluteURI + "/" + originals[i].name + ".mov");
-          originals[i].setProxy(newProxy);
+          // possible matches for the proxy, from the reasonable to the silly
+          var suffixes = [
+            ".mov",
+            ".avi",
+            ".mp4",
+            ".mxf",
+            ".mpg",
+            ".mpeg",
+            ".mkv",
+            ".ogv",
+            ".webm",
+            ".wmv",
+            ".dv",
+            ".omf"
+          ];
+          var attempt = 0;
+          for (var s = 0; s < suffixes.length; s++) {
+            newProxy = new File(proxyFolder.absoluteURI + "/" + originals[i].name + suffixes[s]);
+            if (newProxy.exists) {
+              break
+            }
+          }
+          if (newProxy.exists) {
+            originals[i].setProxy(newProxy);
+          } else {
+            msg += originals[i].name + "\n";
+          }
         }
         originals[i].selected = false;
         originals[i].selected = true;
       } catch (e) {
-        alert(e)
+        alert("had an error" + e)
       }
     }
+    if (msg) {
+      alert("Can't find matching files in\n" + proxyFolder.fsName + "\nfor item:\n" + msg);
+      return false;
+    } else {
+      return true;
+    }
   } else {
+    // no originals were given
     alert("Choose some footage items to add proxies to")
   }
 }
 
 function chooseProxyFolder(startPath) {
   if (startPath) {
-    proxyFolder = startPath.selectDlg("Choose the source folder for the proxies");
+    newFolder = startPath.selectDlg("Choose the source folder for the proxies");
   } else {
-    proxyFolder = Folder.selectDialog("Choose the source folder for the proxies");
+    newFolder = Folder.selectDialog("Choose the source folder for the proxies");
   }
-  writePrefs(proxyFolder);
+  // only update the proxy folder value if a new folder is actually choosen
+  // selectDialog or selectDlg return null if user cancels, but if the proxy folder is already set
+  // we don't want to overwrite it.
+
+  if (newFolder){
+    writePrefs(proxyFolder);
+    proxyFolder = newFolder;
+  }
   return proxyFolder;
 }
 
 function getLastFolder() {
-  prefs = new File(Folder.userData.absoluteURI + "/proxinator.prefs");
+  prefs = new File(prefsFile);
   if (prefs.exists) {
     prefs.open("r");
     lastProxyFolder = new Folder(prefs.read());
     prefs.close();
-    if (lastProxyFolder.exists) {
+    if (lastProxyFolder && lastProxyFolder.exists) {
       return lastProxyFolder;
     }
   }
@@ -52,7 +95,7 @@ function getLastFolder() {
 }
 
 function writePrefs(lastProxyFolder) {
-  prefs = new File(Folder.userData.absoluteURI + "/proxinator.prefs");
+  prefs = new File(prefsFile);
   if (prefs.open("w")) {
     prefs.write(lastProxyFolder);
     prefs.close();
@@ -60,42 +103,68 @@ function writePrefs(lastProxyFolder) {
 }
 
 function buildUI(thisObj) {
+  var proxyFolder = getLastFolder();
   if (thisObj instanceof Panel) {
     pal = thisObj;
   } else {
     pal = new Window("palette", scriptName, undefined, {resizeable: true});
   }
+
   if (pal !== null) {
-    chooseProxyFolderBtn = pal.add("button", [
-      undefined, undefined, undefined, undefined
-    ], "choose proxy folder");
+    chooseProxyFolderBtn = pal.add("button", {
+      x: 4,
+      y: undefined,
+      width: 180,
+      height: 25
+    }, "choose proxy folder");
 
-    lastFolderText = (lastFolder.displayName)
-      ? lastFolder.displayName
-      : "<none>";
+    proxyFolderText = pal.add("statictext", {
+      x: 4,
+      y: undefined,
+      width: 180,
+      height: 25
+    }, lastFolderText, {truncate: "middle"});
 
-    lastFolderStaticText = pal.add("statictext", [
-      undefined, undefined, undefined, undefined
-    ], lastFolderText);
+    proxinateBtn = pal.add("button", {
+      x: 4,
+      y: undefined,
+      width: 180,
+      height: 25
+    }, "set proxies for selected");
 
-    proxynateBtn = pal.add("button", [
-      undefined, undefined, undefined, undefined
-    ], "set proxies for selected");
+    function checkProxyFolderAndUpdateText() {
+      if ( proxyFolder && proxyFolder.fsName) {
+        proxyFolderText.text = proxyFolder.fsName;
+        proxinateBtn.enabled = true;
+      } else {
+        proxyFolderText.text = "<none>";
+        proxinateBtn.enabled = false;
+      }
+    }
+    checkProxyFolderAndUpdateText();
+
+    chooseProxyFolderBtn.onClick = function() {
+      proxyFolder = chooseProxyFolder(proxyFolder);
+      checkProxyFolderAndUpdateText();
+    };
+
+    proxinateBtn.onClick = function() {
+      proxinate(proxyFolder);
+      this.active = false; // stops the button staying activated
+    };
+
+    if (!proxyFolder) {
+      proxinateBtn.enabled = false
+    };
+    // show the panel
+    if (pal instanceof Window) {
+      pal.center();
+      pal.show();
+    } else {
+      pal.layout.layout(true);
+    }
   }
-
-  chooseProxyFolderBtn.onClick = function(){
-    proxyFolder = chooseProxyFolder(lastFolder);
-    lastFolderStaticText.text = (proxyFolder.displayName)
-      ? proxyFolder.displayName
-      : "<none>";
-  }
-  proxynateBtn.onClick = function(){
-    proxynate(proxyFolder);
-  }
-
 }
-
-var lastFolder = getLastFolder();
 // var proxyFolder = chooseProxyFolder(lastFolder);
 // writePrefs(proxyFolder);
 // proxinate(proxyFolder);
