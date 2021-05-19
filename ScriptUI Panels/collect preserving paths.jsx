9111fcf84737efd5f1a62fbcb59b1d6b31735a6e
stib <stib@pureandapplied.com.au>
2021-05-19 17:54:43 +1000

    Fixed weirdness with home folders, choice to open

--- ScriptUI Panels/collect preserving paths.jsx
+++ ScriptUI Panels/collect preserving paths.jsx
@@ -27,18 +27,19 @@ thisScript.buildGUI = function(thisObj) {
     // ======
     var group1 = panel1.add("group", undefined); 
     group1.orientation = "column"; 
-    group1.alignChildren = ["left","center"]; 
+    group1.alignChildren = ["left","top"]; 
     group1.spacing = 10; 
     group1.margins = 0; 
     
     var destFoldrBtn = group1.add("button", undefined, undefined); 
-    destFoldrBtn.text = "Choose"; 
+    destFoldrBtn.text = "Choose";
+    destFoldrBtn.preferredSize.width = 180;
     
     thisObj.destFoldrTxt = group1.add("statictext", undefined, undefined); 
     thisObj.destFoldrTxt.preferredSize.width = 180; 
     thisObj.destFoldrTxt.text = "None"; 
     
-    // PANEL1
+    // do the things button
     // ======
     
     thisObj.doTheThingsBtn = panel1.add("button", undefined, undefined); 
@@ -46,20 +47,27 @@ thisScript.buildGUI = function(thisObj) {
     thisObj.doTheThingsBtn.preferredSize.width = 180; 
     thisObj.doTheThingsBtn.text = "Start"; 
     
-
-
-    thisObj.progressTxt1 = panel1.add("statictext", undefined, undefined); 
-    thisObj.progressTxt1.preferredSize.width = 180;
-    thisObj.progressTxt2 = panel1.add("statictext", undefined, undefined); 
-    thisObj.progressTxt2.preferredSize.width = 180;
-    thisObj.progressTxt3 = panel1.add("statictext", undefined, undefined); 
-    thisObj.progressTxt3.preferredSize.width = 180;
+    var radioPanel = panel1.add("panel", undefined, "After collecting open:");
+    radioPanel.preferredSize.width = 180;
+    radioPanel.alignChildren = ["left", "top"];
+    radioPanel.margins = [10,10,10,4];
+    radioPanel.spacing = 0; 
+    thisObj.origRadio = radioPanel.add( "radiobutton", undefined, "Original project" );
+    newRadio = radioPanel.add( "radiobutton", undefined, "New collected project" );
+    thisObj.origRadio.value = true;
     
     thisObj.progressBar = panel1.add("progressbar", undefined, undefined); 
     thisObj.progressBar.preferredSize.width = 180; 
     thisObj.progressBar.maxvalue = 100; 
     thisObj.progressBar.value = 0; 
     thisObj.progressBar.preferredSize.height = 4; 
+    
+    thisObj.progressTxt1 = panel1.add("statictext", undefined, undefined); 
+    thisObj.progressTxt1.preferredSize.width = 180;
+    thisObj.progressTxt2 = panel1.add("statictext", undefined, undefined); 
+    thisObj.progressTxt2.preferredSize.width = 180;
+    thisObj.progressTxt3 = panel1.add("statictext", undefined, undefined); 
+    thisObj.progressTxt3.preferredSize.width = 180;
     
     //------------------------ Callbacks ---------------------------
     destFoldrBtn.onClick = function(){
@@ -75,7 +83,7 @@ thisScript.buildGUI = function(thisObj) {
         thisObj.progressBar.value = 0;
         thisScript.doTheThings(thisObj);
     }
-
+    
     thisObj.writeMsg = function(theMessage){
         //scrolling text box
         thisObj.progressTxt1.text = thisObj.progressTxt2.text;
@@ -97,8 +105,9 @@ thisScript.name = "Collect with relative paths";
 thisScript.doTheThings = function(UIObj){
     app.beginUndoGroup(thisScript.name);
     var theProj = app.project;
+    theProj.save();
     var originalProjectFile = theProj.file;
-    var destFolder =UIObj.destFolder;
+    var destFolder = UIObj.destFolder;
     if (!destFolder){
         destFolder =  new Folder();
         destFolder = destFolder.selectDlg();
@@ -108,54 +117,69 @@ thisScript.doTheThings = function(UIObj){
             var projItemCount = theProj.numItems;
             var hp = thisScript.getHomePath();
             var rootFolder = thisScript.findCommonFolder(theProj, hp);
+            var errors = [];
             for(var i = 1; i <= projItemCount; i++){
                 var theItem = theProj.item(i);
                 UIObj.writeMsg("" + i + " of " + projItemCount + ": " + theItem.name);
-
+                
                 UIObj.progressBar.value = 100 * i / projItemCount;
                 UIObj.pal.update();
                 if( theItem instanceof FootageItem && theItem.file){
-                    var filePath = theItem.file.toString();
-                    filePath = filePath.replace(/^~/, hp).replace(/^\//, ""); 
-                    var dest = [destFolder, filePath].join("/");
-                    var relativeFolder = filePath;
-                    if (rootFolder){
-                        replaceStr = new RegExp("^" + rootFolder);
-                        var relativeFolder = filePath.replace(replaceStr);
-                    }
-                    dest = [destFolder, relativeFolder].join("/");
-                    var folderTree = relativeFolder.split("/");
-                    var parentFolder = destFolder.toString();
-                    for (dir = 0; dir < folderTree.length -1; dir++){
-                        var newFolder = new Folder([parentFolder, folderTree[dir]].join("/"));
-                        newFolder.create();
-                        parentFolder = newFolder.toString();
-                    }
-                    // $.writeln(dest);
-                    if (!(new File(dest)).exists){
-                        theItem.file.copy(dest);
-                    }
                     try{
-                        theItem.replace(new File(dest));
-                    } catch(e){
-                        UIObj.writeMsg(e);
+                        if (! theItem.footageMissing) {
+                            var filePath = theItem.file.toString();
+                            filePath = filePath.replace(/^~/, hp).replace(/^\//, ""); 
+                            
+                            var relativeFilePath = filePath.replace(/[\r\n]/g, "");
+                            if (rootFolder){
+                                replaceStr = new RegExp("^" + rootFolder);
+                                relativeFilePath = filePath.replace(replaceStr, "");
+                            }
+                            var dest = [destFolder, relativeFilePath].join("/");
+                            
+                            var folderTree = relativeFilePath.split("/");
+                            var parentFolder = destFolder.toString();
+                            for (dir = 0; dir < folderTree.length -1; dir++){
+                                var newFolder = new Folder([parentFolder, folderTree[dir]].join("/"));
+                                if (!newFolder.exists) {
+                                    newFolder.create();
+                                    UIObj.writeMsg("new folder created: " + newFolder.name);
+                                }
+                                parentFolder = newFolder.toString();
+                            }
+                            if (!(new File(dest)).exists){
+                                theItem.file.copy(dest);
+                            }
+                            theItem.replace(new File(dest));
+                        } 
+                    } catch (e) {
+                        UIObj.writeMsg(e.message);
+                        errors.push("error with " + theItem.name + ":\n" + e.message);
                     }
                 }
             }
             var projectFolder = new Folder([destFolder, "AE Project File"].join("/"));
             projectFolder.create();
-            UIObj.writeMsg( "Saving Project File");
-            theProj.save(new File([projectFolder, theProj.file.name].join("/")));
-            UIObj.writeMsg("opening original project file")
-            app.open(originalProjectFile);
-            UIObj.writeMsg( "All done.");
+            UIObj.writeMsg("Saving Project File");
+            var newProjName = theProj.file.name.replace(/\.aep$/i, "") + "_Collected"
+            var collectedProj = new File([projectFolder, newProjName].join("/"));
+            theProj.save(collectedProj);
+            if (UIObj.origRadio.value) {
+                UIObj.writeMsg("opening original project file")
+                app.open(originalProjectFile)
+            };
+            if (errors.length) {
+                alert("There were some errors during export:\n" + errors.join("\n"));
+            } 
+            UIObj.writeMsg("All done.");
+            UIObj.progressBar.value = 0;
         } else {
             UIObj.writeMsg( "No open Project");
         }
     } else { //no dest folder
         UIObj.doTheThingsBtn.enabled = false;
         UIObj.destFoldrTxt = "";
-
+        
     }
     app.endUndoGroup();
 }
@@ -163,12 +187,13 @@ thisScript.doTheThings = function(UIObj){
 thisScript.getHomePath = function(){
     $.writeln($.os);
     if ($.os.match("Windows.*")){
-        var osHome = system.callSystem("cmd.exe /c \"echo %HOMEPATH%\"" );
+        var osHome = system.callSystem('cmd.exe /c "echo %HOMEPATH%"');
+        osHome = osHome.replace(/\\/g, "/");
     } else {
         var osHome = system.callSystem("echo $HOME");
     }
-    var homeFol = new Folder (osHome);
-    return homeFol.toString().replace("%0D%0A", "");
+    // var homeFol = new Folder (osHome);
+    return osHome.replace(/[\r\n]/g, "");
 }
 
 thisScript.findCommonFolder = function(theProj, hp){
@@ -191,72 +216,6 @@ thisScript.findCommonFolder = function(theProj, hp){
         }
     }     
     return commonFolder.join("/");
-}
-
-
-//---------------------------- ui prefs -----------------------------
-thisScript.Preferences = function(scriptName) {
-    // look for preferences for this object
-    // provide a setPref function to allow values to be stored in AE's preferences
-    // scriptName sets the section of the preference file they are saved in.
-    this.prefsName = scriptName;
-    alert ( this.prefsName);
-    parsePref = function(val, prefType) {
-        switch (prefType) {
-            case "integer":
-            case "int":
-            return parseInt(val, 10);
-            case "float":
-            return parseFloat(val);
-            case "bool":
-            return (val === "true");
-            default:
-            return val
-        }
-    }
-    
-    this.setPref = function(anObject) {
-        var currentVal;
-        if (anObject.name){
-            if(anObject.hasOwnProperty('value')){
-                currentVal = anObject.value;
-            } else if (anObject instanceof EditText){
-                currentVal = anObject.text;
-            } else {
-                throw("objects must have a 'text' or 'value' property to set preferences");
-            }
-            
-            if (anObject.savedPref !== currentVal) {
-                anObject.savedPref = currentVal;
-                app.settings.saveSetting(this.scriptName, anObject.name, currentVal);
-            }
-        }
-    }
-    
-    this.getPref = function(anObject){
-        // constructor
-        if (anObject.name ){
-            if (app.settings.haveSetting(this.scriptName, anObject.name)) {
-                // get prefs for UI control     
-                if (anObject instanceof Slider){
-                    anObject.value = anObject.savedPref = parsePref(app.settings.getSetting(anObject.prefsName, anObject.name), "float");   
-                } else if (anObject instanceof Checkbox || anObject instanceof Radiobutton){
-                    anObject.value = anObject.savedPref = parsePref(app.settings.getSetting(anObject.prefsName, anObject.name), "bool");
-                } else if (anObject instanceof EditText ){
-                    anObject.text = anObject.savedPref = parsePref(app.settings.getSetting(anObject.prefsName, anObject.name), "string");
-                } else {
-                    // objects can use specified pref types with the type of the returned result determined by a preftype property
-                    // otherwise the default is a string
-                    anObject.value = anObject.savedPref = anObject.parsePref(app.settings.getSetting(anObject.prefsName, anObject.name), anObject.prefType); 
-                }
-            }
-        } else {
-            throw("objects must have a name to be given prefs.");
-        }
-        
-    }
-    
-    return this;
 }
 
 //--------------------- go ahead and run ----------------------
