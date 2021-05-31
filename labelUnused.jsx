b92ff9dcdaaa89b7098f28e0ab0d0d26b8f18a60
stib <stib@pureandapplied.com.au>
2021-05-31 13:42:00 +1000

    added select all if none selected

--- labelUnused.jsx
+++ labelUnused.jsx
@@ -1,5 +1,9 @@
 // @target aftereffects
-theitems  = app.project.selection;
+theitems = app.project.selection;
+//label all if nothing is selected
+if (theitems.length === 0) {
+    theitems = app.project.items;
+}
 for (var i =0; i< theitems.length; i++){
     var uses = theitems[i].usedIn;
     if (uses && uses.length > 0){
