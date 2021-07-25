c2b4e3fe08c53e3be5f3f298621eba903952302a
stib <stib@pureandapplied.com.au>
2021-07-25 23:11:52 +1000

    license

--- Startup/Autosave Restore.jsx
+++ Startup/Autosave Restore.jsx
@@ -1,4 +1,20 @@
-//@target aftereffects
+// Autosave Restore by stib
+// if AE crashes it finds if there are autosaved copies your project that are newer than the saved one
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+// @target aftereffects
 (function () {
     this.name = "checkForNewerAutoSaves";
     prefName = "lastQuitWasClean";
