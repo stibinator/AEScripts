0446ec28ed1759e17beb6619107b2961352c6f21
stib <stib@pureandapplied.com.au>
2022-04-19 17:24:26 +1000

    Revamped to use native sourcePoinToComp() method

--- ScriptUI Panels/Crop-n-Chop.jsx
+++ ScriptUI Panels/Crop-n-Chop.jsx
@@ -1,5 +1,6 @@
 // @target aftereffects
 // license below
+
 // more: https://blob.pureandapplied.com.au
 
 (function (thisObj) {
@@ -9,15 +10,10 @@
         // returns the left, right, top and bottom most point of a layer or set of layers in comp space
         // if motion is enabled it goes through all the frames looking for the bounding box containing all the motion.
         // note that this it calculates the value for the whole layer, even transparent parts.
-        var boundingBox = { left: null, right: null, bottom: null, top: null };
+        var boundingBox = { left: null, width: null, height: null, top: null };
+        var originalCompTime = theComp.time;
         for (var i = 0; i < theLayers.length; i++) {
             var lyr = theLayers[i];
-            var ptCtrlTL = lyr.property("Effects").addProperty("ADBE Point Control");
-            ptCtrlTL.name = "temp_Top_Left_ToComp";
-            ptCtrlTL.property("point").expression = "let sr = thisLayer.sourceRectAtTime(time); thisLayer.toComp([sr.left, sr.top])";
-            var ptCtrlBR = lyr.property("Effects").addProperty("ADBE Point Control");
-            ptCtrlBR.name = "temp_Bottom_Right_ToComp";
-            ptCtrlBR.property("point").expression = "let sr = thisLayer.sourceRectAtTime(time); thisLayer.toComp([sr.width, sr.height]+[sr.left, sr.top]) ";
             var currTime = theComp.time;
             if (useMotion) {
                 startTime = lyr.inPoint > 0 ? lyr.inPoint : 0;
@@ -30,17 +26,28 @@
             }
             for (t = startTime; t <= endTime; t += theComp.frameDuration) {
                 theComp.time = t;
-                var leftTop = lyr.property("Effects").property("temp_Top_Left_ToComp").property("Point").valueAtTime(t, false);
-                var rightBot = lyr.property("Effects").property("temp_Bottom_Right_ToComp").property("Point").valueAtTime(t, false);
-                if (null === boundingBox.left | boundingBox.left > leftTop[0]){boundingBox.left = leftTop[0]}
-                if (null === boundingBox.right | boundingBox.right < rightBot[0]){boundingBox.right = rightBot[0]}
-                if (null === boundingBox.top | boundingBox.top > leftTop[1]){boundingBox.top = leftTop[1]}
-                if (null === boundingBox.bottom | boundingBox.bottom < rightBot[1]){boundingBox.bottom = rightBot[1]}
+                var srcRect = lyr.sourceRectAtTime(t, true);
+                srcRect.right = srcRect.left + srcRect.width;
+                srcRect.bottom = srcRect.top + srcRect.height;
+                var toCompLeftTop = lyr.sourcePointToComp([srcRect.left, srcRect.top]);
+                var toCompLeftBot = lyr.sourcePointToComp([srcRect.left, srcRect.bottom]);
+                var toCompRightTop = lyr.sourcePointToComp([srcRect.right, srcRect.top]);
+                var toCompRightBot = lyr.sourcePointToComp([srcRect.right, srcRect.bottom]);
+
+                var toCompLeft = Math.min(toCompLeftTop[0], toCompLeftBot[0], toCompRightTop[0], toCompRightBot[0]);
+                var toCompRight = Math.max(toCompLeftTop[0], toCompLeftBot[0], toCompRightTop[0], toCompRightBot[0]);
+                var toCompTop = Math.min(toCompLeftTop[1], toCompLeftBot[1], toCompRightTop[1], toCompRightBot[1]);
+                var toCompBottom = Math.max(toCompLeftTop[1], toCompLeftBot[1], toCompRightTop[1], toCompRightBot[1]);
+                var toCompWidth = toCompRight - toCompLeft;
+                var toCompHeight = toCompBottom - toCompTop;
+                if (null === boundingBox.left | boundingBox.left > toCompLeft) { boundingBox.left = toCompLeft }
+                if (null === boundingBox.width | boundingBox.width < toCompWidth) { boundingBox.width = toCompWidth }
+                if (null === boundingBox.top | boundingBox.top > toCompTop) { boundingBox.top = toCompTop }
+                if (null === boundingBox.height | boundingBox.height < toCompHeight) { boundingBox.height = toCompHeight }
             }
             theComp.time = currTime;
-            lyr.property("Effects").property("temp_Top_Left_ToComp").remove();
-            lyr.property("Effects").property("temp_Bottom_Right_ToComp").remove();
         }
+        theComp.time = originalCompTime;
         return boundingBox;
     }
 
@@ -97,25 +104,83 @@
                 }
             }
         }
-        var width = bounds.right - bounds.left;
-        var height = bounds.bottom - bounds.top;
         if (margin.proportional) {
-            margin.x = margin.value * width;
-            margin.y = margin.value * height;
+            margin.x = margin.value * bounds.width;
+            margin.y = margin.value * bounds.height;
         } else {
-            margin.x = Math.max(margin.value * width, margin.value * height);
+            margin.x = Math.max(margin.value * bounds.width, margin.value * bounds.height);
             margin.y = margin.x;
         }
 
-        theComp.width = Math.max(Math.min(Math.round(width + margin.x), 30000), 4); //min 4px, max 30,000px comp sizes
-        theComp.height = Math.max(Math.min(Math.round(height + margin.y), 30000), 4);
+        theComp.width = Math.max(Math.min(Math.round(bounds.width + margin.x), 30000), 4); //min 4px, max 30,000px comp sizes
+        theComp.height = Math.max(Math.min(Math.round(bounds.height + margin.y), 30000), 4);
         if (reframe) { //move the bounding box to the top left of the frame.
             parentNull.property("Transform").property("Position").setValue([margin.x / 2, margin.y / 2, 0]);
             parentNull.remove();
         }
         for (var s = 1; s < theComp.numLayers; s++) { //recall selection state
-            theComp.layer(s).selected = theComp.layer(s).wasSelected|false;
+            theComp.layer(s).selected = theComp.layer(s).wasSelected | false;
             theComp.layer(s).wasSelected = null;
+        }
+    }
+
+    function findAllUsesOfItem(theItem) {
+        // returns an array of objects containing comp - a composition and an array of indexes, that are the layers that use theComp as their source
+        var result = []
+        var items = app.project.items;
+        for (var itm = 1; itm <= items.length; itm++) {
+            if (items[itm] instanceof CompItem) {
+                var referringLayers = [];
+                var theComp = items[itm];
+                for (var lyr = 1; lyr <= theComp.numLayers; lyr++) {
+                    var theSource = theComp.layers[lyr].source;
+                    if ((theSource) && (theSource.id == theItem.id)) {
+                        referringLayers.push(lyr);
+                    }
+                }
+                if (referringLayers.length > 0) {
+                    result.push({ "comp": theComp, "indexes": referringLayers });
+                }
+            }
+        }
+        return result;
+    }
+
+    function fromComp(theLayer, pos) {
+        var tempPtFX = theLayer.effects.addProperty("ADBE Point Control");
+        tempPtFX.name = "Temp Point for fromComp Script";
+        tempPtFX.property("point").expression = "fromComp([" + pos[0] + ", " + pos[1] + "])";
+        var result = tempPtFX.property("point").value;
+        tempPtFX.remove();
+        return result;
+    }
+
+
+    function adjustLayerPos(theComp, theLayer, bounds) {
+        var originalBounds = findBoundingBox(theComp, theLayer, false);
+        var offsetBounds = {
+            "left": originalBounds.left + bounds.left,
+            "right": originalBounds.left + bounds.right,
+            "top": originalBounds.top + bounds.top,
+            "bottom": originalBounds.top + bounds.bottom
+        }
+        if (theLayer.parent) {
+            childBounds = fromComp(theLayer.parent, [originalBounds.left, originalBounds.top]);
+            newChildBounds = fromComp(theLayer.parent, [offsetBounds.left, offsetBounds.top]);
+            theLayer.transform.position.setValue(theLayer.transform.position.value + [childBounds.left - newChildBounds.left, childBounds.top - newChildBounds.top]);
+        } else {
+            theLayer.transform.position.setValue(theLayer.transform.position.value + [offsetBounds.left - originalBounds.left, offsetBounds.top - originalBounds.top]);
+        }
+    }
+
+
+    function repositionCroppedComp(theComp, bounds) {
+        // adjust the position of a comp that has been cropped in any comps it is used in
+        var compList = findAllUsesOfItem(theComp); //returns array of [{comp, [indexes]}]
+        for (var c = 0; c < compList.length; c++) {
+            for (var i = 0; i < complist[c].indexes.length; i++) {
+                adjustLayerPos(compList[c].comp, complist[c].indexes[i], bounds);
+            }
         }
     }
 
