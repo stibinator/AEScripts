a852b9cf116ef589b3499d8ab8c91336295f772d
stib <stib@pureandapplied.com.au>
2022-07-06 21:37:16 +1000

    fixed JSON polyfill

--- ScriptUI Panels/scriptConsole.jsx
+++ ScriptUI Panels/scriptConsole.jsx
@@ -1,9 +1,3 @@
-
-
-/*
-Code for Import https://scriptui.joonas.me — (Triple click to select): 
-{"activeId":22,"items":{"item-0":{"id":0,"type":"Dialog","parentId":false,"style":{"text":"Import Multiple PDF pages","preferredSize":[0,0],"margins":16,"orientation":"row","spacing":10,"alignChildren":["left","top"],"varName":null,"windowType":"Dialog","creationProps":{"su1PanelCoordinates":false,"maximizeButton":false,"minimizeButton":false,"independent":false,"closeButton":true,"borderless":false,"resizeable":false},"enabled":true}},"item-20":{"id":20,"type":"Group","parentId":23,"style":{"preferredSize":[0,0],"margins":0,"orientation":"column","spacing":10,"alignChildren":["fill","top"],"alignment":null,"varName":null,"enabled":true}},"item-21":{"id":21,"type":"EditText","parentId":20,"style":{"enabled":true,"varName":"textInput","creationProps":{"noecho":false,"readonly":false,"multiline":false,"scrollable":false,"borderless":true,"enterKeySignalsOnChange":true},"softWrap":false,"text":"EditText","justify":"left","preferredSize":[300,26],"alignment":null,"helpTip":"Type here!"}},"item-22":{"id":22,"type":"ListBox","parentId":20,"style":{"enabled":true,"varName":"choiceList","creationProps":{"multiselect":false,"numberOfColumns":"1","columnWidths":"","columnTitles":"","showHeaders":false},"listItems":"Item 1, Item 2, item 3, item 4","preferredSize":[0,100],"alignment":null,"helpTip":null,"selection":[0]}},"item-23":{"id":23,"type":"Group","parentId":0,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-24":{"id":24,"type":"Group","parentId":23,"style":{"enabled":true,"varName":null,"preferredSize":[0,0],"margins":0,"orientation":"row","spacing":10,"alignChildren":["left","center"],"alignment":null}},"item-25":{"id":25,"type":"StaticText","parentId":24,"style":{"enabled":true,"varName":"description","creationProps":{"truncate":"none","multiline":true,"scrolling":true},"softWrap":false,"text":"Script \ndescription\ngoes\nhere","justify":"left","preferredSize":[160,100],"alignment":null,"helpTip":""}}},"order":[0,23,20,21,22,24,25],"settings":{"importJSON":true,"indentSize":false,"cepExport":false,"includeCSSJS":true,"functionWrapper":false,"compactCode":false,"showDialog":true,"afterEffectsDockable":false,"itemReferenceList":"None"}}
-*/
 (function (thisObj) {
     var SCRIPT_NAME = "scriptConsole";
     var VERSION_NUM = 0.1;
@@ -45,6 +39,9 @@ Code for Import https://scriptui.joonas.me — (Triple click to select):
         "16.7,18.7 16.5,19.1 16.1,19.5 15.7,19.7 15.2,19.7 14.7,19.5 14.4,19.1 14.2,18.7 14.2,18.2 14.4,17.7 14.7,17.4 15.2,17.2 15.7,17.2 16.1,17.4 16.5,17.7 16.7,18.2 16.7,18.7 16.7,18.7 18.1,18.7 18.1,17.9 17.7,16.9 16.9,16.1 16.7,16 16.7,4.8 16.5,4.5 16.3,4.1 16,3.9 15.6,3.8 15.2,3.8 14.8,3.9 14.5,4.1 14.3,4.5 14.2,4.8 14.1,16 13.5,16.5 12.9,17.4 12.7,18.4 12.9,19.5 13.5,20.4 14.1,20.8 14.2,25.3 14.3,25.7 14.5,26 14.8,26.2 15.2,26.4 15.6,26.4 16,26.2 16.3,26 16.5,25.7 16.7,25.3 16.7,20.8 17.4,20.4 18,19.5 18.1,18.7 16.7,18.7",
         "8.5,10.7 8.3,11.1 7.9,11.5 7.5,11.7 7,11.7 6.5,11.5 6.2,11.1 6,10.7 6,10.2 6.2,9.7 6.5,9.4 7,9.2 7.5,9.2 7.9,9.4 8.3,9.7 8.5,10.2 8.5,10.7 8.5,10.7 10,10.7 9.9,9.9 9.5,8.9 8.8,8.1 8.5,8 8.5,4.8 8.4,4.5 8.1,4.1 7.8,3.9 7.4,3.8 7,3.8 6.7,3.9 6.3,4.1 6.1,4.5 6,4.8 6,8 5.3,8.5 4.7,9.4 4.5,10.4 4.7,11.5 5.3,12.4 6,12.8 6,25.3 6.1,25.7 6.3,26 6.7,26.2 7,26.4 7.4,26.4 7.8,26.2 8.1,26 8.4,25.7 8.5,25.3 8.5,12.8 9.2,12.4 9.8,11.5 10,10.7 8.5,10.7"
     ];
+    // minified JSON stolen from battleaxe
+    var JSON; JSON || (JSON = {}); (function () { function k(a) { return a < 10 ? "0" + a : a } function o(a) { p.lastIndex = 0; return p.test(a) ? '"' + a.replace(p, function (a) { var c = r[a]; return typeof c === "string" ? c : "\\u" + ("0000" + a.charCodeAt(0).toString(16)).slice(-4) }) + '"' : '"' + a + '"' } function l(a, j) { var c, d, h, m, g = e, f, b = j[a]; b && typeof b === "object" && typeof b.toJSON === "function" && (b = b.toJSON(a)); typeof i === "function" && (b = i.call(j, a, b)); switch (typeof b) { case "string": return o(b); case "number": return isFinite(b) ? String(b) : "null"; case "boolean": case "null": return String(b); case "object": if (!b) return "null"; e += n; f = []; if (Object.prototype.toString.apply(b) === "[object Array]") { m = b.length; for (c = 0; c < m; c += 1)f[c] = l(c, b) || "null"; h = f.length === 0 ? "[]" : e ? "[\n" + e + f.join(",\n" + e) + "\n" + g + "]" : "[" + f.join(",") + "]"; e = g; return h } if (i && typeof i === "object") { m = i.length; for (c = 0; c < m; c += 1)typeof i[c] === "string" && (d = i[c], (h = l(d, b)) && f.push(o(d) + (e ? ": " : ":") + h)) } else for (d in b) Object.prototype.hasOwnProperty.call(b, d) && (h = l(d, b)) && f.push(o(d) + (e ? ": " : ":") + h); h = f.length === 0 ? "{}" : e ? "{\n" + e + f.join(",\n" + e) + "\n" + g + "}" : "{" + f.join(",") + "}"; e = g; return h } } if (typeof Date.prototype.toJSON !== "function") Date.prototype.toJSON = function () { return isFinite(this.valueOf()) ? this.getUTCFullYear() + "-" + k(this.getUTCMonth() + 1) + "-" + k(this.getUTCDate()) + "T" + k(this.getUTCHours()) + ":" + k(this.getUTCMinutes()) + ":" + k(this.getUTCSeconds()) + "Z" : null }, String.prototype.toJSON = Number.prototype.toJSON = Boolean.prototype.toJSON = function () { return this.valueOf() }; var q = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, p = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, e, n, r = { "\u0008": "\\b", "\t": "\\t", "\n": "\\n", "\u000c": "\\f", "\r": "\\r", '"': '\\"', "\\": "\\\\" }, i; if (typeof JSON.stringify !== "function") JSON.stringify = function (a, j, c) { var d; n = e = ""; if (typeof c === "number") for (d = 0; d < c; d += 1)n += " "; else typeof c === "string" && (n = c); if ((i = j) && typeof j !== "function" && (typeof j !== "object" || typeof j.length !== "number")) throw Error("JSON.stringify"); return l("", { "": a }) }; if (typeof JSON.parse !== "function") JSON.parse = function (a, e) { function c(a, d) { var g, f, b = a[d]; if (b && typeof b === "object") for (g in b) Object.prototype.hasOwnProperty.call(b, g) && (f = c(b, g), f !== void 0 ? b[g] = f : delete b[g]); return e.call(a, d, b) } var d, a = String(a); q.lastIndex = 0; q.test(a) && (a = a.replace(q, function (a) { return "\\u" + ("0000" + a.charCodeAt(0).toString(16)).slice(-4) })); if (/^[\],:{}\s]*$/.test(a.replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, "@").replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, "]").replace(/(?:^|:|,)(?:\s*\[)+/g, ""))) return d = eval("(" + a + ")"), typeof e === "function" ? c({ "": d }, "") : d; throw new SyntaxError("JSON.parse"); } })();
+
     // do the hoo-hah
     var prefs = new myPreferences(SCRIPT_NAME);
     var firstRun = prefs.getPref("ScriptConsole-FirstRun", true);
@@ -225,7 +222,7 @@ Code for Import https://scriptui.joonas.me — (Triple click to select):
             { name: "description", multiline: true, alignment: ['top', 'left'] }
         );
         // scriptInfoStaticText.multiline = true;
-        scriptInfoStaticText.preferredSize = { "width": infoWidth - 20, "height": infoHeight -20 };
+        scriptInfoStaticText.preferredSize = { "width": infoWidth - 20, "height": infoHeight - 20 };
         // description.preferredSize.height = 100; 
         var btnGrp = rightGroup.add('Group', undefined);
         btnGrp.alignment = ['right', 'bottom'];
@@ -297,10 +294,10 @@ Code for Import https://scriptui.joonas.me — (Triple click to select):
                 // dialog.layout.layout(true);
             }
             var h = Math.max(dialog.bounds.height, 40);
-            choiceList.bounds.height = h  - 60;
+            choiceList.bounds.height = h - 60;
             group2.bounds.height = h;
             group1.bounds.height = h;
-            rightGroup.bounds.height = h ;
+            rightGroup.bounds.height = h;
             scriptInfoStaticText.bounds.height = h - 80;
             descriptionGroup.bounds.height = h - 80;
             btnGrp.bounds.top = h - 60;
